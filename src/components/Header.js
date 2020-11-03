801036f9c31a1142461356859be8e53406f06160
chenuvi <chenuvi@outlook.com>
2020-11-03 22:05:12 +0800

    feat: header 添加 link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <>
-            <h2>Header</h2>
+            <img src=''></img>
+            <nav>
+                <Link to='/'>首页</Link>
+                <Link to='/history'>上传历史</Link>
+                <Link to='/about'>关于</Link>
+            </nav>
         </>
     );
 }
