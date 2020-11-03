ee732b730e9c574a13f6dc04764edb97db83d91d
chenuvi <chenuvi@outlook.com>
2020-11-03 21:30:01 +0800

    feat: 设置路由

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './pages/About'
+import History from './pages/History'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Header />
+
+      <Switch>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/history'  component={History}></Route>
+        <Route path='/'  component={About}></Route>
+      </Switch>
+
+      <Footer />
     </div>
   );
 }
