305a9ae0f76b67b5e2f607b63c7c511787720ce4
chenuvi <chenuvi@outlook.com>
2020-11-05 11:21:48 +0800

    feat ： login 组件本地功能

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,109 @@
-import React, { useRef } from 'react'
-import { observer } from 'mobx-react'
-import { useStores } from '../stores'
+import React from 'react'
+import {Form, Input, Button} from 'antd';
+import styled from 'styled-components'
 
-const Component = observer(() => {
-  const { AuthStore } = useStores()
-  const inputRef = useRef()
+const Wrapper = styled.div`
+  max-width:600px;
+  margin:30px auto;
+  box-shadow:2px 2px 4px 0 rgba(0,0,0,0.2);
+  border-radius:4px;
+  padding:20px;
+`
+const Title = styled.h1`
+  text-align:center;
+  margin-bottom:30px;
+`
 
-  const bindChange = e => {
-    console.log(inputRef.current.value);
-    AuthStore.setUsername(inputRef.current.value)
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const Component = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const validateUsername = (rule,value) => {
+    if(/\W/.test(value)) return Promise.reject('只能是字母数字下划线');
+    if(value.length < 4 || value.length > 10) return Promise.reject('长度为4～10个字符');
+    console.log('value: ', value);
+    console.log('rule: ', rule);
+    Promise.resolve()
   }
 
   return (
-    <>
-      <h1>login:{AuthStore.values.username}</h1>
-      <input onChange={bindChange} ref={inputRef} />
-    </>
+    <Wrapper>
+      <Title>登录</Title>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '输入用户名',
+            },
+            {
+              validator:validateUsername
+            }
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              whitespace:false,
+              message: '输入密码',
+            },
+            {
+              min:4,
+              message:'最少4个字符'
+            },
+            {
+              max:16,
+              message:'最多16个字符'
+            },
+          ]}
+        >
+          <Input.Password/>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
   );
-})
+};
 
 export default Component;
