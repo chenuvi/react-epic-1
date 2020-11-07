c8dd23fd4c3099a6ebea8a78b76505225186bf29
chenuvi <chenuvi@outlook.com>
2020-11-07 22:10:59 +0800

    pref: antd 组件上传文件

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,29 +1,48 @@
 import React, { useRef } from 'react'
 import { useStores } from '../stores'
 import { observer } from 'mobx-react'
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
 
 const Component = observer(() => {
     const { ImageStore } = useStores()
     const ref = useRef()
-
-    const bindChange = () => {
-        console.log('ref.current1: ', ref.current);
-        if (ref.current.files.length > 0) {
-            ImageStore.setFile(ref.current.files[0])
-            ImageStore.setFilename(ref.current.files[0].name)
+    const props = {
+        name: 'file',
+        showUploadList:false,
+        multiple: false,
+        beforeUpload: file => {
+            console.log('file: ', file);
+            ImageStore.setFile(file)
+            ImageStore.setFilename(file.name)
             ImageStore.upload()
                 .then((serverFile) => {
                     console.log('serverFile上传成功: ', serverFile)
-                }).catch( error => { 
+                }).catch(error => {
                     console.log('error in Uploader component: ', error);
                 })
+            return false
         }
-        window.file = ref.current
     }
+
     return (
         <>
-            <h2>上传文件</h2>
-            <input type='file'  ref={ref}  onChange={bindChange} />
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                    Support for a single . Strictly prohibit from uploading company data or other band files
+                </p>
+            </Dragger>,
+            <h2>上传结果</h2>
+            {
+               ImageStore.serverFile ?  <><b>{ImageStore.filename}</b><br />{ImageStore.serverFile.attributes.url.attributes.url}</> : null
+            }
+            
         </>
     );
 })
