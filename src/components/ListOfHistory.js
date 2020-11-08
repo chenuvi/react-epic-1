e4f18bedee652306469c5530b969fddd03f86c2b
chenuvi <chenuvi@outlook.com>
2020-11-09 01:06:26 +0800

    feat: 历史图片加载

---
+++ src/components/ListOfHistory.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { observer } from 'mobx-react'
+import { useStores } from '../stores'
+import { List, message, Spin } from 'antd';
+
+import InfiniteScroll from 'react-infinite-scroller';
+
+const HistoryList = observer(() => {
+    const { HistoryStore } = useStores()
+    console.log('HistoryStore.list: ', HistoryStore.list);
+
+    const loadMore = () => {
+        HistoryStore.find()
+    }
+
+    return (
+        <>
+            <h2>HistoryList</h2>
+            <InfiniteScroll
+                initialLoad={true}
+                pageStart={0}
+                loadMore={loadMore}
+                hasMore={!HistoryStore.isLoading && HistoryStore.hasMore}
+                useWindow={true}
+            >
+                <List
+                    dataSource={ HistoryStore.list}
+                    renderItem={item => (
+                        <List.Item key={item.id}>
+                            <div ><img style={{height:'100px'}} src={item.attributes.url.attributes.url}></img></div>
+                            <div>
+                                <h5>{item.attributes.filename}</h5>
+                            </div>
+                            <div>
+                                <a target="_blank" href={item.attributes.url.attributes.url}>{item.attributes.url.attributes.url}</a>
+                            </div>
+                        </List.Item>
+                    )}
+                >
+                    {HistoryStore.isLoading && HistoryStore.hasMore && (
+                        <div>
+                            <Spin tip="加载中" />
+                        </div>
+                    )}
+                </List>
+            </InfiniteScroll>
+        </>
+    );
+})
+
+export default HistoryList;
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ListOfHistory from '../components/ListOfHistory'
 
 function History() {
     return (
         <>
             <h2>History</h2>
+            <ListOfHistory></ListOfHistory>
         </>
     );
 }
